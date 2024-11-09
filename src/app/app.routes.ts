655407e7d64cd2d4c587aa24140c0aee2a7964bd
Natalia Bezerra <b1z3rr4@gmail.com>
2024-11-09 15:07:26 -0300

    chore(home): create a home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/Home/Home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
